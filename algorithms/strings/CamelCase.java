ecc01dcb435b5451c68fcedcc5e07ae87cee5db0
Samuel Giannotti <samuelgiannotti@hotmail.com>
2017-05-04 19:41:41 -0300

    Update CamelCase.java

--- algorithms/strings/CamelCase.java
+++ algorithms/strings/CamelCase.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class CamelCase {
 
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
-        
-        int count = 0;
-        char[] arr = s.toCharArray();
-        if (arr.length > 0)
-        	count++;
-        for (int i = 0; i < arr.length; i++) {
-        	if (arr[i] < 97)
-        		count++;
-        }
-        
-        System.out.println(count);
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+
+		int count = 0;
+		char[] arr = s.toCharArray();
+		if (arr.length > 0)
+			count++;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < 97)
+				count++;
+		}
+
+		System.out.println(count);
 	}
 
 }
