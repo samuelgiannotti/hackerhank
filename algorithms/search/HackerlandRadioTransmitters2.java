d67170c1d78717e5a7e50eef8e97ac602c3cbb66
Samuel Giannotti <samuelgiannotti@hotmail.com>
2017-05-07 18:24:45 -0300

    Update HackerlandRadioTransmitters2.java

--- algorithms/search/HackerlandRadioTransmitters2.java
+++ algorithms/search/HackerlandRadioTransmitters2.java
@@ -5,46 +5,6 @@ import java.util.Scanner;
 
 public class HackerlandRadioTransmitters2 {
 
-	static void calculateRadios(int[] x, int k) {
-		int numOfRadios = 0;
-		if (x.length == 1) {
-			System.out.println(1);
-			return;
-		}
-		Arrays.sort(x);
-		int lastRadioPos = -k-1;
-		int nextSugRadPos = x[0]+k;
-		for (int i = 0; i < x.length; i++) {
-			int actual = x[i];
-			if (i+1 < x.length) {
-				if ((x[i]+k < x[i+1] && lastRadioPos+k < x[i]) || nextSugRadPos <= x[i] || nextSugRadPos < x[i+1]) { //se o prox e o ant mto distante
-					numOfRadios++;
-					lastRadioPos = x[i];
-					nextSugRadPos = x[i] + (k*2);
-
-					if (x[i+1] <= lastRadioPos+k) {
-						for (int l = i; l < x.length; l++) {
-							if (x[l] > lastRadioPos+k) {
-								nextSugRadPos = x[l] + k;
-								break;
-							} else if (x[l] == nextSugRadPos) {
-								break;
-							}
-						}
-					} else {
-						nextSugRadPos = x[i+1] + (k);
-					}
-						
-				}	
-			} else {
-				if (x[i] > lastRadioPos+k)
-					numOfRadios++;
-			}
-		}
-			
-		System.out.println(numOfRadios);
-	}
-	
 	public static void main(String[] args) {
 		 Scanner in = new Scanner(System.in);
 
@@ -57,7 +17,7 @@ public class HackerlandRadioTransmitters2 {
 	        }
 	        Arrays.sort(a);
 	      
-	        int ans = 0;
+	        int ret = 0;
 	        
 	        for (int i = 0; i < n; i++) {
 	          int j = i;
@@ -67,10 +27,10 @@ public class HackerlandRadioTransmitters2 {
 	          while (j + 1 < n && a[j + 1] - a[i] <= k)
 	            j++;
 	          i = j;
-	          ans++;
+	          ret++;
 	        }
 	        
-	        System.out.println(ans);
+	        System.out.println(ret);
 	}
 
 }
