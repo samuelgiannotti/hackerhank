0d8344756b19d2cba9b338ab04625ab6f28de84f
Samuel Giannotti <samuelgiannotti@hotmail.com>
2017-05-18 21:09:56 -0300

    Add files via upload

--- dataStructures/LinkedList/Compare2Linkedlists.java
+++ dataStructures/LinkedList/Compare2Linkedlists.java
@@ -1,5 +1,12 @@
 package hackerhank.dataStructures.LinkedList;
 
+/**
+ * 
+ * @author Samuel Giannotti
+ * 
+ * https://www.hackerrank.com/challenges/compare-two-linked-lists
+ *
+ */
 public class Compare2Linkedlists {
 
 	  public class Node {
@@ -8,11 +15,34 @@ public class Compare2Linkedlists {
 	  }
 	
 	  
-	  int CompareLists(Node headA, Node headB) {
-		    // This is a "method-only" submission. 
-		    // You only need to complete this method 
-		  return 0;
-		}	  
+	  /*
+	  Compare two linked lists A and B
+	  Return 1 if they are identical and 0 if they are not. 
+	  Node is defined as 
+	  class Node {
+	     int data;
+	     Node next;
+	  }
+	*/
+	int CompareLists(Node headA, Node headB) {
+	    if (headA == null && headB == null)
+	        return 1;
+	    Node tempA = headA.next;
+	    Node tempB = headB.next;
+	    while (tempA != null) {
+	        if (tempB == null) {
+	            return 0;
+	        }
+	        if (tempA.data != tempB.data) {
+	            return 0;
+	        }
+	        tempA = tempA.next;
+	        tempB = tempB.next;
+	    }
+	    if (tempA == null && tempB != null)
+	        return 0;
+	    return 1;
+	}
 	  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
