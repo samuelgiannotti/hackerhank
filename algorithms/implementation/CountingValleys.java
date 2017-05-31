4a2964e6c165f0bb3448892bc9380a3ea194401d
Samuel Giannotti <samuelgiannotti@hotmail.com>
2017-05-31 09:53:25 -0300

    Update CountingValleys.java

--- algorithms/implementation/CountingValleys.java
+++ algorithms/implementation/CountingValleys.java
@@ -12,23 +12,23 @@ import java.util.Scanner;
 public class CountingValleys {
 
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String s = in.next();
-        int result = 0;
-        int level = 1;
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-        	if (chars[i] == 'U') {
-        		level++;
-        		if (level == 1) {
-        			result++;
-        		}
-        	} else {
-        		level--;
-        	}
-        }
-        
-        System.out.println(result);
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		String s = in.next();
+		int result = 0;
+		int level = 1;
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == 'U') {
+				level++;
+				if (level == 1) {
+					result++;
+				}
+			} else {
+				level--;
+			}
+		}
+
+		System.out.println(result);
 	}
 }
