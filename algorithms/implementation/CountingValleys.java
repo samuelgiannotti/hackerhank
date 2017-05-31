74ac2e5ba70778b01b1975c2721bdfda2a27904f
Samuel Giannotti <samuelgiannotti@hotmail.com>
2017-05-31 09:51:45 -0300

    Add files via upload

---
+++ algorithms/implementation/CountingValleys.java
@@ -0,0 +1,34 @@
+package hackerhank.algorithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Samuel Giannotti
+ * 
+ * https://www.hackerrank.com/challenges/counting-valleys
+ *
+ */
+public class CountingValleys {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = in.next();
+        int result = 0;
+        int level = 1;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+        	if (chars[i] == 'U') {
+        		level++;
+        		if (level == 1) {
+        			result++;
+        		}
+        	} else {
+        		level--;
+        	}
+        }
+        
+        System.out.println(result);
+	}
+}
